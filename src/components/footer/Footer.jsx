6713b93694919aa8adf0aaf00eed5774ca0828c1
zeyad2900 <zeyad.mohamedjs@gmail.com>
2023-10-26 23:15:34 +0200

    zlapya

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,13 @@
 import React from "react";
 
 const Footer = () => {
-    return;
+    return(
+        <div className=" h-screen bg-black w-full">
+            <h1 className="text-white">
+                mohamed
+            </h1>
+        </div>
+    )
 };
 
 export default Footer;
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -7,14 +7,14 @@ import img from "../../assets/hero.jpg";
 const Hero = () => {
     return (
         <div className="h-screen w-full bg-black">
-            <div className="container mx-auto flex sm:justify-center justify-between h-full">
+            <div className="container mx-auto flex-col xl:flex-row flex sm:justify-center justify-between h-full">
                 <div className=" ml-16  mt-48 h-64 flex flex-col items-center md:items-start">
                     <p className="text-white font-thin text-2xl leading-loose">ALWAYS THE BEST WAY TO</p>
                     <h1 className="text-white font-black text-6xl md:text-8xl">Building</h1>
                     <h1 className="text-white font-black text-6xl md:text-8xl">Choices</h1>
                 </div>
-                <div className=" hidden xl:block flex-1 ml-16 mt-8 overflow-hidden h-3/4 rounded-3xl ">
-                    <Image alt="" src={img} width={6080} height={6080}></Image>
+                <div className=" xl:block ml-16 mt-8 overflow-hidden w-full h-4/5 rounded-3xl ">
+                    <Image alt="" src={img} style={{ width: "100%", height: "100%" }}></Image>
                 </div>
             </div>
         </div>
