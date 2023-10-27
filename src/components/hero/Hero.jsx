8ab5aca7f8b73c90a245536d814cad4b7791052a
zeyad2900 <zeyad.mohamedjs@gmail.com>
2023-10-27 02:25:59 +0200

    animation

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,18 +3,22 @@
 import Image from "next/image";
 import React from "react";
 import img from "../../assets/hero.jpg";
+import "./Hero.css";
 
 const Hero = () => {
     return (
         <div className="h-screen w-full bg-black">
-            <div className="container mx-auto flex-col xl:flex-row flex sm:justify-center justify-between h-full">
-                <div className=" ml-16  mt-48 h-64 flex flex-col items-center md:items-start">
-                    <p className="text-white font-thin text-2xl leading-loose">ALWAYS THE BEST WAY TO</p>
-                    <h1 className="text-white font-black text-6xl md:text-8xl">Building</h1>
-                    <h1 className="text-white font-black text-6xl md:text-8xl">Choices</h1>
+            <div className="container mx-auto flex-col xl:flex-row flex justify-between items-center xl:items-start h-full">
+                <div className=" ml-16  mt-5 mb-32 xl:mt-48 h-64 flex flex-col items-center md:items-start">
+                    <p className="scale-in-center text-white font-thin text-2xl leading-loose">ALWAYS THE BEST WAY TO</p>
+                    <h1 className="scale-in-center text-white font-black text-6xl md:text-8xl">Building</h1>
+                    <h1 className="scale-in-center text-white font-black text-6xl md:text-8xl">Choices</h1>
+                    <button className="jello-horizontal bg-white text-black mt-20 py-5 px-20 text-4xl flex justify-center items-center font-bold hover:bg-cyan-500 hover:text-white transition-colors duration-300 rounded-full">
+                        Read more
+                    </button>
                 </div>
-                <div className=" xl:block ml-16 mt-8 overflow-hidden w-full h-4/5 rounded-3xl ">
-                    <Image alt="" src={img} style={{ width: "100%", height: "100%" }}></Image>
+                <div className="xl:block xl:ml-16 mt-8 overflow-hidden w-full h-4/5 rounded-3xl ">
+                    <Image className="kenburns-bottom" alt="" src={img} style={{ width: "100%", height: "100%" }}></Image>
                 </div>
             </div>
         </div>
--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -1,37 +1,19 @@
 import React from "react";
 import { FaFacebookF } from "react-icons/fa6";
-import { FaXTwitter } from "react-icons/fa6";
-import { FaTwitter } from "react-icons/fa6";
-import { FaTelegram } from "react-icons/fa6";
-import { FaLinkedinIn } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
 
 const SideBar = () => {
     return (
-        <div>
-            <ul className="fixed top-1/2 right-3 rounded-full overflow-hidden transform -translate-y-1/2 bg-cyan-500">
+        <div className="fixed top-1/2 right-3 transform -translate-y-1/2 z-20">
+            <ul>
                 <a href="#">
-                    <li className="text-white p-4 hover:bg-black cursor-pointer flex justify-center items-center transition-colors duration-300">
+                    <li className="text-white p-4 hover:bg-black cursor-pointer flex justify-center items-center transition-colors duration-300 bg-cyan-500 rounded-full mb-2">
                         <FaFacebookF></FaFacebookF>
                     </li>
                 </a>
                 <a href="#">
-                    <li className="text-white p-3 hover:bg-black cursor-pointer flex justify-center items-center transition-colors duration-300">
-                        <FaXTwitter></FaXTwitter>
-                    </li>
-                </a>
-                <a href="#">
-                    <li className="text-white p-3 hover:bg-black cursor-pointer flex justify-center items-center transition-colors duration-300">
-                        <FaTwitter></FaTwitter>
-                    </li>
-                </a>
-                <a href="#">
-                    <li className="text-white p-3 hover:bg-black cursor-pointer flex justify-center items-center transition-colors duration-300">
-                        <FaLinkedinIn></FaLinkedinIn>
-                    </li>
-                </a>
-                <a href="#">
-                    <li className="text-white p-3 hover:bg-black cursor-pointer flex justify-center items-center transition-colors duration-300">
-                        <FaTelegram></FaTelegram>
+                    <li className="text-white p-4 hover:bg-black cursor-pointer flex justify-center items-center transition-colors duration-300 bg-cyan-500 rounded-full mb-2">
+                        <FaInstagram></FaInstagram>
                     </li>
                 </a>
             </ul>
