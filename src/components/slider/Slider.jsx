28534c1f4daa0fac3f8ae4fa9fab74703648ee12
zeyad2900 <zeyad.mohamedjs@gmail.com>
2023-10-29 13:46:18 +0200

    update

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import src from "../../assets/hero.jpg";
+import src2 from "../../assets/download.jpeg"
 import "./Slide.css";
 import { FaArrowRightLong } from "react-icons/fa6";
 import { FaArrowLeftLong } from "react-icons/fa6";
@@ -19,7 +20,7 @@ function Slider() {
         {
             id: 2,
             content: "hotel",
-            img: src,
+            img: src2,
         },
         {
             id: 3,
@@ -29,7 +30,7 @@ function Slider() {
         {
             id: 4,
             content: "hotel",
-            img: src,
+            img: src2,
         },
         {
             id: 5,
@@ -39,7 +40,7 @@ function Slider() {
         {
             id: 6,
             content: "hotel",
-            img: src,
+            img: src2,
         },
         {
             id: 7,
@@ -49,7 +50,7 @@ function Slider() {
         {
             id: 8,
             content: "hotel",
-            img: src,
+            img: src2,
         },
         {
             id: 9,
@@ -59,7 +60,7 @@ function Slider() {
         {
             id: 10,
             content: "hotel",
-            img: src,
+            img: src2,
         },
         {
             id: 11,
@@ -84,8 +85,8 @@ function Slider() {
 
     return (
         <div className=" my20 text-white bg-black">
-            <div className=" container h-full  mx-auto w-full flex flex-col justify-start items-center">
-                <div className="flex justify-center items-center ">
+            <div className=" container h-screen  mx-auto w-full flex flex-col justify-start items-center">
+                <div className="flex justify-center items-center w-full h-full ">
                     <button
                         className=" mr-5 bg-cyan-500 text-white px-4 py-2 rounded-full transition duration-200 ease-in-out hover:bg-cyan-700 active:bg-purple-900 focus:outline-none"
                         onClick={() => {
@@ -95,7 +96,7 @@ function Slider() {
                         <FaArrowLeftLong></FaArrowLeftLong>
                     </button>
 
-                    <div className=" h-1/2 mt-0 w-2/3 overflow-hidden rounded-xl ">
+                    <div className=" h-1/2 xl:h-full mt-0 w-2/3 overflow-hidden rounded-xl ">
                         <Image alt={arrOfImg[imgNum].id} src={arrOfImg[imgNum].img} style={{ width: "100%", height: "100%" }}></Image>
                     </div>
                     <button
