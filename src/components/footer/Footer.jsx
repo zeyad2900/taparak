c8e5b85275b39304c681937f10df3c9ac48c96b4
zeyad2900 <zeyad.mohamedjs@gmail.com>
2023-10-31 17:07:06 +0200

    footer rsponsev

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,15 +11,15 @@ const Footer = () => {
         <div className=" bg-black my-20">
             <div className="container mx-auto">
                 <hr className="text white" />
-                <div className="text-white grid grid-cols-3 w-full mt-20">
-                    <div className=" flex flex-col items-center justify-center">
+                <div className="text-white grid grid-cols-1 md:grid-cols-3 w-full mt-20">
+                    <div className=" flex flex-col items-center justify-center sm:mb-10">
                         <Image alt="logo" src={src} width={250} height={75}></Image>
                         <div className=" flex items-center justify-center w-full">
                             <FaPhoneAlt className=" rounded-full w-10 h-10 p-2 bg-cyan-500 text-white"></FaPhoneAlt>
                             <h1 className=" ml-4 font-black text-4xl">16341</h1>
                         </div>
                     </div>
-                    <ul className=" flex flex-col items-center justify-center">
+                    <ul className=" flex flex-col items-center justify-center sm:mb-10">
                         {navItems.map((ele) => {
                             return (
                                 <a href={ele.id} key={ele.id}>
@@ -28,7 +28,7 @@ const Footer = () => {
                             );
                         })}
                     </ul>
-                    <ul className=" flex flex-col items-center justify-center">
+                    <ul className=" flex flex-col items-center justify-center sm:mb-10">
                         {sideNavItems.map((ele) => {
                             return (
                                 <div key={ele.id} className=" flex items-center justify-center mb-5">
