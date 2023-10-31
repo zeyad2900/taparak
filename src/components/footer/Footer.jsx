ee08faa30745d1655eedf3f6ce0a387dbe569ab5
zeyad2900 <zeyad.mohamedjs@gmail.com>
2023-10-31 14:18:15 +0200

    zeyad

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     return(
         <div className=" h-screen bg-black w-full my-20">
             <h1 className="text-white">
-                mohamed
+                zeyad
             </h1>
         </div>
     )
